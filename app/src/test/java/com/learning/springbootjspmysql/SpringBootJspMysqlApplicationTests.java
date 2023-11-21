2b8f85ed405c469bc5d296fb2ad38034c4f193b8
Nidhin Surendran <nidhins@gmail.com>
2023-11-21 11:12:19 +0530

    dev container for java

---
+++ app/src/test/java/com/learning/springbootjspmysql/SpringBootJspMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootjspmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
